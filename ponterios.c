b4d282cb73d0b23257f2e21ec94114d82b5ade6c
evandro1994 <evandrofcf@hotmail.com>
2024-09-04 19:23:55 -0300

    Create ponterios.c
    Usando as funções:
    -malloc
    -realloc
    -free

---
+++ ponterios.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+int *p, i;
+
+p = malloc (10*sizeof(int));
+if (p == NULL){
+	printf ("Espaço insuficiente");
+	exit(1);
+}else{
+	
+printf("Primeiros 10 numeros.\n");
+for(i=0; i<10; i++){
+	p[i] = i+10;
+	printf (" %d-%d\n",i,p[i]);
+	}
+	
+p = realloc(p,20*sizeof(int));
+
+printf("Realaconado para mais 10 números.\n");
+for(i=10; i<20; i++){
+	p[i] = i+20;
+	printf (" %d-%d\n",i,p[i]);
+}
+printf("Todos os 20 numeros alocados. \n");
+for(i=0; i<20; i++)
+		printf(" %d-%d\n",i,p[i]);
+free(p);
+}
+system("pause");
+return (0);
+}
